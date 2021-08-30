5a7770afd6bb23af2cd7993525dd74f819114f9a
Jerry J <78483514+jerryjarvela@users.noreply.github.com>
2021-08-30 09:50:42 +0300

    Dominion of kings assignment

---
+++ DominionOfKings/DominionOfKings/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+int score;
+int provinceValue = 6;
+int duchyValue = 3;
+int estateValue = 1;
+
+Console.Write("Enter amount of provinces: ");
+int provinces = Convert.ToInt32(Console.ReadLine());
+int provinceScore = provinces * provinceValue;
+
+Console.Write("Enter amount of duchies: ");
+int duchies = Convert.ToInt32(Console.ReadLine());
+int duchyScore = duchies * duchyValue;
+
+Console.Write("Enter amount of estates: ");
+int estates = Convert.ToInt32(Console.ReadLine());
+int estateScore = estates * estateValue;
+
+score = provinceScore + duchyScore + estateScore;
+Console.WriteLine("Total points: " + score);
